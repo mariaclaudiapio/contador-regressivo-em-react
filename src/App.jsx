4886e166dcb46e20d9d24f46ee9e6a3bdb6693b7
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-02-21 16:08:19 -0300

    estruturando os principais elementos em jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
 
+import Birthday from "./assets/birthday.jpg";
+
+import Title from "./components/Title";
+import Counter from "./components/Counter";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Contador Regressivo</h1>
+    <div className="App" style={{ backgroundImage: `url(${Birthday})` }}>
+      <div className="container">
+        <Title title="Contagem regressiva para o meu aniversário" />
+        <div className="countdown-container">
+          <Counter title="Dias" number={2} />
+          <Counter title="Horas" number={2} />
+          <Counter title="Minutos" number={2} />
+          <Counter title="Segundos" number={2} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,10 @@
+const Counter = ({ title, number }) => {
+  return (
+    <div className="counter">
+      <p className="counter-number">{number}</p>
+      <h3 className="counter-title">{title}</h3>
+    </div>
+  );
+};
+
+export default Counter;
