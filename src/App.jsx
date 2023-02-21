fd13da27db0c45ec7cded0ae0b92bd95c578cdad
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-02-21 16:51:33 -0300

    codificação da lógica de contagem regressiva

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Birthday from "./assets/birthday.jpg";
 
 import Title from "./components/Title";
 import Counter from "./components/Counter";
+import useCountDown from "./hooks/useCountDown";
 
 function App() {
+  const [day, hour, minute, second] = useCountDown("Jun 24, 2023 10:00:00");
+
   return (
     <div className="App" style={{ backgroundImage: `url(${Birthday})` }}>
       <div className="container">
         <Title title="Contagem regressiva para o meu aniversário" />
         <div className="countdown-container">
-          <Counter title="Dias" number={2} />
-          <Counter title="Horas" number={2} />
-          <Counter title="Minutos" number={2} />
-          <Counter title="Segundos" number={2} />
+          <Counter title="Dias" number={day} />
+          <Counter title="Horas" number={hour} />
+          <Counter title="Minutos" number={minute} />
+          <Counter title="Segundos" number={second} />
         </div>
       </div>
     </div>
