e66977553c1c950461827102ceba00a8704c8fdb
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-02-23 18:50:21 -0300

    criando a funcionalidade de troca de cor do fundo do cronômetro e dos títulos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import { useContext } from "react";
 
 import Birthday from "./assets/birthday.jpg";
+import { CountdownContext } from "./context/CountdownContext";
 
 function App() {
+  const { event } = useContext(CountdownContext);
+
+  let eventImage = null;
+
+  if (event) eventImage = event.image;
+
   return (
-    <div className="App" style={{ backgroundImage: `url(${Birthday})` }}>
+    <div
+      className="App"
+      style={
+        eventImage
+          ? { backgroundImage: `url(${eventImage})` }
+          : { backgroundImage: `url(${Birthday})` }
+      }
+    >
       <div className="container">
         <Outlet />
       </div>
